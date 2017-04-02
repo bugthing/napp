086cacb758cafb4c5b0c07a68add5f78d6bc4e2f
Benjamin Martin <ben.martin@smartpension.co.uk>
2017-04-02 20:31:24 +0100

    Mix new

---
+++ lib/napp.ex
@@ -0,0 +1,18 @@
+defmodule Napp do
+  @moduledoc """
+  Documentation for Napp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Napp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/napp_test.exs
@@ -0,0 +1,8 @@
+defmodule NappTest do
+  use ExUnit.Case
+  doctest Napp
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
